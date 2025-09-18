9614554d8e281cd3d40dc291871fcdaa70fce8a9
dasilva.chandra <chandrad176@gmail.com>
2025-09-18 18:21:48 +0700

    update: 2025-09-18 18:21:46 | +49 -522 (~37%)

--- zabbix/healthcheck-web.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-# Try to fetch local nginx front page
-status=$(curl -s -o /dev/null -w '%{http_code}' http://127.0.0.1:8080/)
-[ "$status" = "200" ] || { echo "HTTP $status"; exit 1; }
-echo OK
