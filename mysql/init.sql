7b1414117956b84c805fb73dc6020b8c06244e46
dasilva.chandra <chandrad176@gmail.com>
2025-09-18 15:35:44 +0700

    update: 2025-09-18 15:35:43 | +10 -0 (~0%)

--- mysql/init.sql
+++ mysql/init.sql
@@ -7,3 +7,13 @@ CREATE USER 'rep1'@'%' IDENTIFIED BY 'abcdef';
 GRANT REPLICATION SLAVE, REPLICATION CLIENT ON *.* TO 'rep1'@'%';
 
 FLUSH PRIVILEGES;
+
+-- User & database untuk Zabbix
+DROP DATABASE IF EXISTS zabbix;
+CREATE DATABASE zabbix CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+
+DROP USER IF EXISTS 'zabbix'@'%';
+CREATE USER 'zabbix'@'%' IDENTIFIED BY 'abcdef';
+
+GRANT ALL PRIVILEGES ON zabbix.* TO 'zabbix'@'%';
+FLUSH PRIVILEGES;
